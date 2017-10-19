540074ff1c49a2ac6f278ca70fde570ecc30a812
Ryan <ryan.d.elliott@gmail.com>
2017-10-19 17:59:45 -0600

    user controller making API call and getting data since last update

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,14 @@
 const request = require('request');
+const baseApiUrl = 'https://www.googleapis.com/fitness/v1/users/me/dataSources/derived%3Acom.google.step_count.delta%3Acom.google.android.gms%3Aestimated_steps/datasets/';
+const keyParam = '?key=' + process.env.FIT_API_KEY;
 
 const getFitData = (req, res) => {
-	const apiURL = 'https://www.googleapis.com/fitness/v1/users/me/dataSources/derived%3Acom.google.step_count.delta%3Acom.google.android.gms%3Aestimated_steps/datasets/1507096800000000000-1507186365767000000?key=AIzaSyBuk-lBklIXJa-RMbLUgv73c5IbBbgvWD8'
+	// call google fit api and get steps since last login
+	let startTime = convertToNanoS(res.locals.currentUser.fitData.lastUpdate);
+	let endTime = convertToNanoS(Date.now());
+	console.log('start: ' + startTime);
+	console.log('endTime: ' + endTime);
+	const apiURL = `${baseApiUrl}${startTime}-${endTime}${keyParam}`;
 	
 	const options = {
 		url: apiURL,
@@ -28,6 +35,12 @@ function totalSteps(data){
 		total += point.value[0].intVal;
 	});
 	return total;
+}
+
+function convertToNanoS(dateStr){
+	const date = new Date(dateStr);
+	const dateNano = date.getTime() * 1000000;
+	return dateNano;
 }
 
 module.exports = { getFitData };
