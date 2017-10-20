6f0e4832cd65f0e889f31bac07e85e280fccc937
Ryan <ryan.d.elliott@gmail.com>
2017-10-20 17:16:31 -0600

    Pull app controller out of routes into own file

--- config/routes.js
+++ config/routes.js
@@ -5,20 +5,7 @@ router.route('/')
 	.get(controllers.statics.home);
 
 router.route('/app')
-	.get(authenticatedUser, (req, res) => {
-		// process goal data
-		let goals = res.locals.currentUser.goals.map((goal) => {
-			let targetDate = new Date(goal.target.date);
-			return {
-				goalId: goal._id,
-				titleText: `Reach ${goal.target.name} by ${targetDate.toLocaleDateString()}`,
-				bodyText: `From ${goal.start.distance} to ${goal.target.distance}`,
-				complete: goal.complete
-			};
-		});
-		// TODO: sort by date or completion, maybe have completed in separate array to be hidden/expanable
-		res.render('app', {user: res.locals.currentUser, goalCards: goals});
-	});
+	.get(authenticatedUser, controllers.app.renderApp);
 
 router.route('/user/totalmiles')
 	.get(authenticatedUser, controllers.user.getFitData);
--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,4 @@
 module.exports.auth = require('./auth');
 module.exports.statics = require('./statics');
 module.exports.user = require('./user');
+module.exports.app = require('./app');
