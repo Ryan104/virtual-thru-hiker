8eedc22091ac8d5a82842132255afaccea7432cb
Ryan <ryan.d.elliott@gmail.com>
2017-10-20 12:51:17 -0600

    Display blank prog bar on app page

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,13 +1,22 @@
 'use strict';
 
+var totalMiles = 0;
+var trailTotal = 2174.6;
+
 $(document).ready(function () {
 	console.log('js/jquery loaded');
 
 	// get user total miles
 	$.ajax({
-		url: '/user/totalMiles',
+		url: '/user/totalmiles',
 		success: function success(res) {
-			return console.log(res);
+			console.log(res);
+			totalMiles = res.totalDistance;
+			var percentComplete = (totalMiles / trailTotal).toFixed(2);
+			if (percentComplete < 1) {
+				percentComplete = 1;
+			}
+			console.log('% complete: ' + percentComplete);
 		}
 	});
 
--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,27 @@
+let totalMiles = 0;
+const trailTotal = 2174.6;
 
 $(document).ready(() => {
 	console.log('js/jquery loaded');
 
+
 	// get user total miles
 	$.ajax({
-		url: '/user/totalMiles',
-		success: (res) => console.log(res)
+		url: '/user/totalmiles',
+		success: (res) => {
+			console.log(res);
+			totalMiles = res.totalDistance;
+			let percentComplete = (totalMiles/trailTotal).toFixed(2);
+			if (percentComplete < 1){
+				percentComplete = 1;
+			}
+			console.log('% complete: ' + percentComplete);
+
+		}
 	});
 
 	// get user goals
 
 	// get upcomming trailmarks
-	
+
 });
